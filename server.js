4b75119295a6e2d9b1fbb249752e51699fcf1f7a
CrimeMaster <gtirkey300@gmail.com>
2023-12-30 15:59:05 +0530

    fifth commit

--- server.js
+++ server.js
@@ -39,8 +39,8 @@ app.get("/page", authorization, (req, res) => {
     });
   });
 
-app.listen(port, () => {
-    mongoose.connect(process.env.MONGOdb_URL)
+app.listen(4000, () => {
+    mongoose.connect('mongodb+srv://admin123:admin123@cluster0.xy5mwr8.mongodb.net/?retryWrites=true&w=majority')
     .then(() => console.log(`Server running on http://localhost:${process.env.port}`))
     .catch(error => console.log(error))
 })
